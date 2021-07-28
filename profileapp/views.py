ccea041bf28c5ce87ede249901abadb4a742a1b2
codeblue25 <chois1031@gmail.com>
2021-07-28 10:25:31 +0900

    210728
    decorator로 profile update view 접근 조건 설정

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,13 +1,18 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
+from profileapp.decorators import profile_ownership_required
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
 
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class ProfileCreateView(CreateView):
     model = Profile
     form_class = ProfileCreationForm
@@ -15,10 +20,12 @@ class ProfileCreateView(CreateView):
     template_name = 'profileapp/create.html'
 
     def form_valid(self, form):
-        form.instance.user = self.request.user
+        form.instance.user = self.request.user    # 유저를 특정해줌
         return super().form_valid(form)
 
 
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
 class ProfileUpdateView(UpdateView):
     model = Profile
     form_class = ProfileCreationForm
