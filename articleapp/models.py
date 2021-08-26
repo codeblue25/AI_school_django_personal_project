880530c1fc677256bab5c4e655789e921b89bbef
codeblue25 <chois1031@gmail.com>
2021-08-26 16:13:22 +0900

    210826
    기본 경로(home) 설정, header 탭 디자인 수정, Project 설정 없이 게시글쓰기 허용

--- articleapp/models.py
+++ articleapp/models.py
@@ -9,7 +9,7 @@ class Article(models.Model):
     writer = models.ForeignKey(User, on_delete=models.SET_NULL,
                                related_name='article', null=True)
     project = models.ForeignKey(Project, on_delete=models.SET_NULL,
-                                related_name='article', null=True)
+                                related_name='article', null=True, blank=True) # 프로젝트 설정 없이 게시글 쓸 수 있음
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=True)
     content = models.TextField(null=True)
